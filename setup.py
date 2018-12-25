0f1bcee26f211b22394c8902a9a6658e1084f87a
Arshad Kazmi <arshadkazmi42@gmail.com>
2018-12-25 15:57:50 +0530

    build 1.0.5

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("README", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="ak-phrase.py",
-    version="1.0.4",
+    version="1.0.5",
     author="Arshad Kazmi",
     author_email="arshadkazmi42@gmail.com",
     description="Generates permutations of all sentences, using list of words",
